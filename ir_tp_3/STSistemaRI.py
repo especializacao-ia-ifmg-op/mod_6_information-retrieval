94fc47d4849af303c05e666f712eaef48b37b8a3
Fernando dos Santos Alves Fernandes <81314458+Fsaf-GitHub-Pro@users.noreply.github.com>
2024-04-10 20:04:06 -0300

    Add files via upload

--- ir_tp_3/STSistemaRI.py
+++ ir_tp_3/STSistemaRI.py
@@ -22,10 +22,13 @@ import re
 # indexer.save_index()
 
 def to_assessment():
-    print(f'{st.session_state.checker}')
+    print(f'[to_assessment] {st.session_state.checker}')
     
 def type_of_query():
-    print(f'{str(st.session_state.radio)}')
+    print(f'[type_of_query]{str(st.session_state.radio)}')
+    
+def specific_context():
+    print(f'[specific_context]{str(st.session_state.context_checker)}')
 
 st.title('SIRIE - Sistema de Recuperação de Informação sobre Enfermagem')
 st.write('Benvindo(a) ao seu buscador sobre Enfermagem!')
@@ -35,11 +38,7 @@ contexto = 'enfermagem'
 query = st.text_input("O que você deseja pesquisar? ")
 
 with st.sidebar:
-    st.sidebar.title("Ferramentas de pessquisa")
-    #assessment = True
-    assessment = st.sidebar.checkbox("Avaliar o Sistema de RI", value=False, on_change=to_assessment, key="checker")
-    
-    relevant_urls = st.sidebar.text_area(label="URLs relevantes:")
+    st.sidebar.title("Ferramentas de pesquisa")
 
     #query_type = 1 # 1 = Interseção; 2 = União.
     query_type_option = st.sidebar.radio("Estratégia de processamento das consultas:",
@@ -51,16 +50,26 @@ with st.sidebar:
         query_type = 1
     else:
         query_type = 2
+    
+    if st.sidebar.checkbox("Especificar contexto", value=False, on_change=specific_context, key="context_checker"):
+        contexto = st.text_input('Contexto específico: ', 'estomaterapia')
+    else:
+        contexto = 'enfermagem'
+
+    #assessment = True
+    assessment = st.sidebar.checkbox("Avaliar o Sistema de RI", value=False, on_change=to_assessment, key="checker")
+    
+    relevant_urls = st.sidebar.text_area(label="URLs relevantes:")        
 
 st.markdown('--------')
                 
-#relevants = list(relevant_urls.split(sep=','))
-relevants = list(re.split(',| |',relevant_urls)) # usando regex!
+relevants = list(relevant_urls.replace('\n','').replace('\t','').replace(' ','').split(sep=','))
+#relevants = list(re.split(',| |',relevant_urls)) # usando regex!
 
 if query or st.button('Buscar'):
     searcher = Buscador("index-"+contexto+".json")
     results = searcher.new_search(query.lower(), query_type) # passar o tipo de query!
-    st.write(f"Resultados: ")
+    st.write(f"{len(results)} resultados encontrados: ")
     for r in results:
        st.markdown('- ' + r)
     if assessment and len(relevants) > 0 and len(results) > 0:
