4ab167959ae49b0b56438c31695091afdac46659
Fernando dos Santos Alves Fernandes <81314458+Fsaf-GitHub-Pro@users.noreply.github.com>
2024-04-10 11:23:53 -0300

    Add files via upload

--- ir_tp_3/url.py
+++ ir_tp_3/url.py
@@ -2,5 +2,5 @@ class Url:
     def __init__(self, url, soup) -> None:        
         self.url = url # Só um atributo de informação!
         self.page = soup.find('title')
-        self.titles = soup.find_all('h2') # Textos do site original
+        self.titles = soup.find_all('[h1, h2]')
         self.links = soup.find_all('a', href=True)
